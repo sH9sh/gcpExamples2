9d23a1245c403d68c174dde2cac472e9790f65e1
Seán Hester <sean.hester@zinkworks.com>
2024-05-23 15:03:33 +0100

    second commit

---
+++ src/main/java/com/gcp/gcpExample2/GcpExample2Application.java
@@ -0,0 +1,13 @@
+package com.gcp.gcpExample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpExample2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcp/gcpExample2/GcpExample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcp.gcpExample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpExample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
